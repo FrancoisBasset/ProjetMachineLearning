a8ebf5d15819eee4d815442500000cc051c29f2e
DIJOUX Damien <dijouxd1993@gmail.com>
2020-05-12 12:04:22 +0200

    3D

---
+++ MLGroupe1/Assets/Scripts/UpTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform[] trainSpheresTransforms;
+
+    public Transform[] testSpheresTransforms;
+
+    public void TrainAndTest()
+    {
+        Debug.Log("Training and Testing");
+
+        // Créer dataset_inputs
+        // Créer dataest_expected_outputs
+
+        // Create Model
+
+        // Train Model
+
+        // For each testSphere : Predict 
+        foreach (var testSpheresTransform in testSpheresTransforms)
+        {
+            bool isUped = IsUped();
+            Debug.Log($" --> {isUped}");
+            if (isUped)
+            {
+                testSpheresTransform.position += Vector3.up * 2;
+            }
+
+        }
+    }
+
+    private bool IsUped()
+    {
+        var random = new System.Random();
+
+        return (random.Next() > 0) ;
+    }
+}
